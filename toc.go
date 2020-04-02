f8b01dcb12ac90431050b93699a7a532b1066b67
Antoine Mercadal <antoine.mercadal@gmail.com>
2020-04-02 14:41:27 -0700

    fixed: generate code that lints

--- toc.go
+++ toc.go
@@ -53,7 +53,7 @@ func (root *assetTree) funcOrNil() string {
 }
 
 func (root *assetTree) writeGoMap(w io.Writer, nident int) {
-	fmt.Fprintf(w, "&bintree{%s, map[string]*bintree{", root.funcOrNil())
+	fmt.Fprintf(w, "{%s, map[string]*bintree{", root.funcOrNil())
 
 	if len(root.Children) > 0 {
 		io.WriteString(w, "\n")
@@ -87,7 +87,7 @@ func (root *assetTree) WriteAsGoMap(w io.Writer) error {
 	Func     func() (*asset, error)
 	Children map[string]*bintree
 }
-var _bintree = `)
+var _bintree = &bintree`)
 	root.writeGoMap(w, 0)
 	return err
 }
